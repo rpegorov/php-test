8ac99bfa52a8e1cb9b3669d3ac8d76d1ec89cca9
Rostislav Egorov <rpegorov@icloud.com>
2023-03-23 12:22:05 +0300

    refactoring smsService

--- test4.php
+++ test4.php
@@ -5,17 +5,15 @@ use Library\SmsService;
 
 class Notification {
 
-    // в конструкторе оставляем только номер для отправки
-    private $smsTo;     
-    public function __construct(string $smsTo) {
-        $this->smsTo = $smsTo;
+    private $smsService;
+    private $smsTo; 
+
+    public function __construct(SmsService $smsService) {
+        $this->smsService = $smsService;
     }
-    
-    // логика отправки сообщения переносится в метод notify, где в случае необходимости можно поменять используемую библиотеку SmsService
-    // на другую с минимальными исправлениями 
-    public function notify(SmsService $smsService, string $message) {
-        $smsService->send($this->smsTo, $message);
+
+    public function notify(string $message) {
+        $this->smsService->send($this->smsTo, $message);
     }
 }
-
 ?>
