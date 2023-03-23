fb52514abde4711a9f30d1500c447fadda6e1ed2
Rostislav Egorov <rpegorov@icloud.com>
2023-03-23 08:27:10 +0300

    исправлен тест4

--- test4.php
+++ test4.php
@@ -5,19 +5,17 @@ use Library\SmsService;
 
 class Notification {
 
-    private $smsService;
-    private $smsTo; //нехватало ; можно еще явно определить тип string
-    public function __construct(string $smsApiKey, string $smsFrom, string $smsTo) {
-        // не понятная расстановка брейсеров, может так и прнято, но не удобно читать
-        // не знаю на сколько корректно использовать разное наименование функций в одном коде __
-        $this->smsService = new SmsService($smsApiKey, $smsFrom);
+    // в конструкторе оставляем только номер для отправки
+    private $smsTo;     
+    public function __construct(string $smsTo) {
         $this->smsTo = $smsTo;
     }
-
-    public function notify(string $message) {
-        $this->smsService->send($this->smsTo, $message);
+    
+    // логика отправки сообщения переносится в метод notify, где в случае необходимости можно поменять используемую библиотеку SmsService
+    // на другую с минимальными исправлениями 
+    public function notify(SmsService $smsService, string $message) {
+        $smsService->send($this->smsTo, $message);
     }
 }
 
-// пропущен закрывающий тег 
 ?>
